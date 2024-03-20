5338723e2ea8588c99dc7802d16064736e5050a0
j <93398891+basaaj@users.noreply.github.com>
2024-03-19 19:36:12 -0500

    Add error message when passwords don't match

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const password_1 = document.querySelector('#password');
 const password_2 = document.querySelector('#password-confirm');
+const error_msg = document.querySelector('#password-error');
 
 password_2.addEventListener('keyup', ()=> {
     if(password_2.value !== password_1.value) {
+        password_1.classList.add('error');
         password_2.classList.add('error');
+        error_msg.classList.remove('hidden');
     }
 
     else {
+        password_1.classList.remove('error');
         password_2.classList.remove('error');
+        error_msg.classList.add('hidden');
     }
 });
